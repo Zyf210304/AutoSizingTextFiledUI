1e10ef008700612ea15f88bfdb318a0bd53c027a
张亚飞 <672233580@qq.com>
2021-06-22 13:49:21 +0800

    飞

--- AutoSizingTextFiledUI/ContentView.swift
+++ AutoSizingTextFiledUI/ContentView.swift
@@ -9,13 +9,150 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct Home: View {
+    
+    @State var text = ""
+    
+    //auto updating textBox height...
+    @State var containHeight: CGFloat = 0
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            VStack {
+               
+                AutoSizingTF(hint: "EnterMessage", text: $text, containerHeight: $containHeight, onEnd: {
+                    
+                    //done when keyboard closed
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                })
+                    .padding(.horizontal)
+                    .frame(height: containHeight < 120 ? containHeight : 120)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .padding()
+            }
+            .navigationTitle("Input Accessory View")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.primary.opacity(0.04).ignoresSafeArea())
+        }
+    }
+}
+
+// building autosize text filed...
+struct AutoSizingTF: UIViewRepresentable {
+    
+    var hint: String
+    @Binding var text: String
+    @Binding var containerHeight: CGFloat
+    
+    var onEnd : () -> ()
+    
+    func makeCoordinator() -> Coordinator {
+        
+        return AutoSizingTF.Coordinator(parent:  self)
+        
+    }
+    
+    
+    func makeUIView(context: Context) -> UITextView {
+        
+        let  textView = UITextView()
+        
+        textView.text = hint
+        textView.textColor = .gray
+        textView.backgroundColor = .clear
+        textView.font = .systemFont(ofSize: 20)
+        
+        textView.delegate = context.coordinator
+        
+        //input accessory view..
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        toolBar.barStyle = .default
+        
+        //since we need done at right...
+        //so using another item as spacer...
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: context.coordinator, action: #selector(context.coordinator.closeKeyBoard))
+        
+        toolBar.items = [spacer, doneButton]
+        toolBar.sizeToFit()
+        
+        textView.inputAccessoryView = toolBar
+        
+        return textView
+    }
+    
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        
+        DispatchQueue.main.async {
+            
+            if containerHeight == 0 {
+                
+                containerHeight = uiView.contentSize.height
+            }
+        }
+    }
+    
+    
+    class Coordinator: NSObject, UITextViewDelegate {
+        
+        //to read all parent properties
+        var  parent: AutoSizingTF
+        
+        init(parent: AutoSizingTF) {
+            
+            self.parent = parent
+        }
+        
+        //keyboard close @objc function...
+        @objc func closeKeyBoard() {
+            
+            parent.onEnd()
+        }
+        
+        
+        func textViewDidBeginEditing(_ textView: UITextView) {
+            
+            if textView.text == parent.hint {
+                
+                textView.text = ""
+                textView.textColor = UIColor(Color.primary)
+            }
+        }
+        
+        
+        func textViewDidChange(_ textView: UITextView) {
+            
+            parent.text = textView.text
+            parent.containerHeight = textView.contentSize.height
+        }
+        
+        //on end checking if textbox is empty
+        //if so then put hint
+        func textViewDidEndEditing(_ textView: UITextView) {
+            
+            if textView.text == "" {
+                
+                textView.text = parent.hint
+                textView.textColor = .gray
+            }
+        }
+        
     }
 }
