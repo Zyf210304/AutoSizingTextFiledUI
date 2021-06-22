2a7cf17fa643bf46da67c8a2aa1f1e8cb0c76f94
张亚飞 <672233580@qq.com>
2021-06-22 11:14:28 +0800

    Initial Commit

---
+++ AutoSizingTextFiledUI/AutoSizingTextFiledUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoSizingTextFiledUIApp.swift
+//  AutoSizingTextFiledUI
+//
+//  Created by 张亚飞 on 2021/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AutoSizingTextFiledUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
